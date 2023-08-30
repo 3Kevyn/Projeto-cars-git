120e0c420fe0b84a3613d615358fa9c838535bca
Kevyn <kevyncouto34@gmail.com>
2023-08-30 19:33:28 -0300

    adicionado filtro para barra de pesquisa

---
+++ filter.js
@@ -0,0 +1,36 @@
+const MyUl = document.getElementById('Carros-Modelos');
+const MyLi = MyUl.getElementsByTagName("li");
+
+function Filtrar() {
+    var inputElement,
+        filter,
+        a,
+        i,
+        span,
+        txtValue,
+        count = 0;
+    inputElement = document.getElementById('inputBusca');
+    filter = inputElement.value.toUpperCase();
+
+    for(i=0;i<MyLi.length;i++)
+    {
+        a = MyLi[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if(txtValue.toLocaleUpperCase().indexOf(filter) > -1 )
+        {
+            MyLi[i].style.display = "";
+            count++;
+            span = MyLi[i].querySelector(".Name-Mark");
+            if(span)
+            {
+                span.innerHTML = txtValue.replace(new RegExp(filter, "gi"),(match) =>{
+                    return "<strong>" + match + "</storng>"
+                } )
+            }
+        }
+        else{
+            MyLi[i].style.display = "none";
+            count = 0;
+        }
+    }
+}
